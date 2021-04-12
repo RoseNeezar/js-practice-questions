dc19a89ed14d470c02fb97c1779cf4b9d8800263
Neezar <rforlyffe@gmaicl.com>
2021-04-12 22:44:14 +0800

    findTwoSum optimize

--- findTwoSum/index.js
+++ findTwoSum/index.js
@@ -3,18 +3,25 @@ const findTwoSum = (arr, sum) => {
   if (arr.length < 2) {
     return false;
   }
-  let p1 = 0;
-  let p2 = 0;
-  for (let i = 0; i < arr.length - 1; i++) {
-    p1 = arr[i];
-    for (let j = i + 1; j < arr.length; j++) {
-      p2 = arr[j];
-      let max = p1 + p2;
-      if (max === sum) {
-        return [i, j];
-      }
+  // for (let i = 0; i < arr.length - 1; i++) {
+  //   const numToFind = sum - arr[i];
+  //   for (let j = i + 1; j < arr.length; j++) {
+  //     if (arr[j] === numToFind) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  const map = {};
+  for (let i = 0; i < arr.length; i++) {
+    const ntf = map[arr[i]];
+    if (ntf >= 0) {
+      return [ntf, i];
+    } else {
+      const res = sum - arr[i];
+      map[res] = i;
     }
   }
   return false;
 };
+
 module.exports = findTwoSum;
