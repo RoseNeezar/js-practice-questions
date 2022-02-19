e51da15f7b0c227e3004eb809436b8e09465895e
Neezar <rforlyffe@gmaicl.com>
2022-02-19 22:49:58 +0800

    longest substring no repeat - fast

--- longestSubstringNoRepeat/index.js
+++ longestSubstringNoRepeat/index.js
@@ -1,21 +1,41 @@
 // brute force
-const longestSubstring = (str) => {
-  let maxLength = 0;
-  for (let i = 0; i < str.length; i++) {
-    let charMap = {};
-    let currentLength = 0;
-    for (let j = i; j < str.length; j++) {
-      let currentChar = str[j];
+// const longestSubstring = (str) => {
+//   let maxLength = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     let charMap = {};
+//     let currentLength = 0;
+//     for (let j = i; j < str.length; j++) {
+//       let currentChar = str[j];
+//       if (!charMap[currentChar]) {
+//         currentLength++;
+//         charMap[currentChar] = true;
+//         maxLength = Math.max(maxLength, currentLength);
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+//   return maxLength;
+// };
 
-      if (!charMap[currentChar]) {
-        currentLength++;
-        charMap[currentChar] = true;
-        maxLength = Math.max(maxLength, currentLength);
-      } else {
-        break;
-      }
+// optimize
+const longestSubstring = (str) => {
+  if (str.length <= 1) return str.length;
+
+  const seenChar = {};
+  let left = 0;
+  let longest = 0;
+
+  for (let right = 0; right < str.length; right++) {
+    const currentChar = str[right];
+    const prevSeenChar = seenChar[currentChar];
+
+    if (prevSeenChar >= left) {
+      left = prevSeenChar + 1;
     }
+    seenChar[currentChar] = right;
+    longest = Math.max(longest, right - left + 1);
   }
-  return maxLength;
+  return longest;
 };
 module.exports = longestSubstring;
--- longestSubstringNoRepeat/index.test.js
+++ longestSubstringNoRepeat/index.test.js
@@ -1,12 +1,11 @@
 const longestSubstring = require("./index");
 
 test("should return true", () => {
+  expect(longestSubstring("abcbdca")).toEqual(4);
+});
+test("should return true", () => {
   expect(longestSubstring("rithmschool")).toEqual(7);
 });
 test("should return true", () => {
   expect(longestSubstring("thisishowwedoit")).toEqual(6);
-});
-
-test("should return true", () => {
-  expect(longestSubstring("abcbdca")).toEqual(4);
 });
